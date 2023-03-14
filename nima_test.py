f83058fecbb96e71bf5c1040eba77355d5032132
Shirzadg <shirzadg@ualberta.ca>
2023-03-13 18:20:39 -0600

    ‘demo’

--- nima_test.py
+++ nima_test.py
@@ -1,52 +1,70 @@
 import sys
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication, QWidget, QCalendarWidget, QComboBox, QGridLayout, QHBoxLayout, QLabel, QPushButton, QVBoxLayout
-from PyQt5.QtWebKitWidgets import QWebView
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import *
 
-class MyWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
 
-    def initUI(self):
-        # Create the Google Maps box
-        map_box = QWebView(self)
-        api_key = "YOUR_API_KEY"  # Replace with your own API key
-        map_box.setUrl(QUrl(f"https://www.google.com/maps/embed/v1/place?key={api_key}&q=Space+Needle,Seattle+WA"))
+        # set window title and size
+        self.setWindowTitle("Google Maps in PyQt5")
+        self.setGeometry(100, 100, 800, 600)
 
-        # Create the button
-        button = QPushButton("Click me", self)
-        button.setToolTip("This is a button")
+        # create a QWebEngineView widget to display the map
+        self.map_box = QGroupBox("Map")
+        self.browser = QWebEngineView()
+        map_layout = QVBoxLayout()
+        map_layout.addWidget(self.browser)
+        self.map_box.setLayout(map_layout)
+        self.map_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.map_box.setStyleSheet("QGroupBox { padding: 10px; }")
 
-        # Create the calendar
-        calendar = QCalendarWidget(self)
+        # load the map using the Google Maps Embed API
+        map_url = "https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Edmonton"
+        self.browser.setHtml(f"<iframe id='map_iframe' frameborder='0' style='border:0' src='{map_url}' allowfullscreen></iframe>")
+        self.map_box.resizeEvent = self.on_map_box_resized  # connect resizeEvent to slot
 
-        # Create the drop-down menu
-        drop_down = QComboBox(self)
-        drop_down.addItem("1")
-        drop_down.addItem("2")
-        drop_down.addItem("3")
-        drop_down.addItem("4")
-        drop_down.addItem("5")
+        # create a group box for the other elements
+        self.other_box = QGroupBox("Other Elements")
+        other_layout = QVBoxLayout()
 
-        # Create the layout
-        hbox = QHBoxLayout()
-        hbox.addWidget(QLabel("Drop-down menu:"))
-        hbox.addWidget(drop_down)
+        # create a button
+        self.button = QPushButton("Click me")
+        other_layout.addWidget(self.button)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(map_box)
-        vbox.addWidget(button)
-        vbox.addWidget(calendar)
-        vbox.addLayout(hbox)
+        # create a dropdown menu
+        self.dropdown = QComboBox()
+        self.dropdown.addItems(["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"])
+        other_layout.addWidget(self.dropdown)
 
-        self.setLayout(vbox)
+        # create a calendar widget
+        self.calendar = QCalendarWidget()
+        self.calendar.setSelectedDate(QDate.currentDate())
+        other_layout.addWidget(self.calendar)
 
-        self.setGeometry(300, 300, 500, 400)
-        self.setWindowTitle("My Window")
-        self.show()
+        self.other_box.setLayout(other_layout)
 
-if __name__ == '__main__':
+        # create a main widget and add the map and other elements to it
+        main_widget = QWidget(self)
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.map_box)
+        main_layout.addWidget(self.other_box)
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
+
+    def on_map_box_resized(self, event):
+        # get the new size of the map box
+        width = event.size().width()
+        height = event.size().height()
+
+        # update the width and height of the iframe tag
+        self.browser.page().runJavaScript(f"document.getElementById('map_iframe').width='{width}px';")
+        self.browser.page().runJavaScript(f"document.getElementById('map_iframe').height='{height}px';")
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MyWindow()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
